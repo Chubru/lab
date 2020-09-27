911329a785ad89548ef78af4eccee930e4081f60
Chubru <71672903+Chubru@users.noreply.github.com>
2020-09-28 00:48:06 +0300

    Create Lab.cpp

---
+++ Lab _2/Lab.cpp
@@ -0,0 +1,63 @@
+// Lab.cpp: определяет точку входа для консольного приложения.
+//
+
+
+#include <stdio.h>
+#include <ctype.h>
+
+
+
+//#pragma warning(disable : 4996)  
+
+//#pragma warning(disable : 4996)  
+
+int checkLetter(char d){
+  if((d>='a')&&(d<='z')){
+    return 1;
+  }
+  if((d>='A')&&(d<='Z')){
+    return 1;
+  }
+  return 0;
+}
+
+
+
+
+int main()
+{
+
+  char dat[100000];
+  int want=1;
+  //scanf("%d",&len);
+  printf("%s","Enter string\n");
+
+  //scanf("%100[0-9a-zA-Z ]", &dat);
+  gets_s(dat);
+  printf("%s","Enter arg\n");
+  scanf("%d", &want);
+  int kol=0;
+  int len=0;
+  for(int i=0;i<sizeof(dat)-1;){
+    
+    if(checkLetter(dat[i])!=0){
+      len++;
+
+    }
+    else if (len !=0){
+      if(len==want){
+        kol++;
+
+      }
+      len=0;
+    }
+    if(dat[i]=='\0'){
+      break;
+    }
+    i++;
+
+  }
+  printf("%s %d\n","Kol =",kol);
+
+  return 0;
+}
