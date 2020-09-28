96303156b52739d36343d910dfe4dfdb8db6f888
Chubru <71672903+Chubru@users.noreply.github.com>
2020-09-28 23:08:51 +0300

    Update Lab.cpp

--- Lab _2/Lab.cpp
+++ Lab _2/Lab.cpp
@@ -3,19 +3,21 @@
 
 
 
+//#pragma warning(disable : 4996)  
 
 
 
 //проверяет является ли переданный символ буквой.
+//проверяет является ли переданный символ буквой.
 
 int checkLetter(char d){
-  if((d>='a')&&(d<='z')){//		коды строчных букв идут последовательно  
-    return 1;
-  }
-  if((d>='A')&&(d<='Z')){//		коды заглавных букв идут последовательно 
-    return 1;
-  }
-  return 0;
+	if((d>='a')&&(d<='z')){//		коды строчных букв идут последовательно  
+		return 1;
+	}
+	if((d>='A')&&(d<='Z')){//		коды заглавных букв идут последовательно 
+		return 1;
+	}
+	return 0;
 }
 
 
@@ -23,38 +25,37 @@ int checkLetter(char d){
 
 int main()
 {
+	char dat;
+	int want=1;
+	//scanf("%d",&len);
 
-  char dat[100000];
-  int want=1;
-  //scanf("%d",&len);
-  printf("%s","Enter string\n");
+	//scanf("%100[0-9a-zA-Z ]", &dat);
 
-  //scanf("%100[0-9a-zA-Z ]", &dat);
-  gets(dat);
-  printf("%s","Enter arg\n");
-  scanf("%d", &want);
-  int kol=0;
-  int len=0;
-  for(int i=0;i<sizeof(dat)-1;){
-    
-    if(checkLetter(dat[i])!=0){
-      len++;
+	printf("%s","Enter arg\n");
+	scanf("%d", &want);
+	printf("%s","Enter string\n");
+	int kol=0;
+	int len=0;
+	for(int i=0;;i++){
+		dat=getchar();
+		if(checkLetter(dat)!=0){
+			len++;
 
-    }
-    else if (len !=0){
-      if(len==want){
-        kol++;
+		}
+		else if (len !=0){
+			if(len==want){
+				kol++;
 
-      }
-      len=0;
-    }
-    if(dat[i]=='\0'){
-      break;
-    }
-    i++;
+			}
+			len=0;
+		}
+		if(dat=='\n'){
+			if(i!=0){
+			break;
+			}
+		}
+	}
+	printf("%s %d\n","Kol =",kol);
 
-  }
-  printf("%s %d\n","Kol =",kol);
-
-  return 0;
+	return 0;
 }
