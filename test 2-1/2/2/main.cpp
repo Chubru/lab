945e7cd01fc624c56f2299e3bca9f1d18b2a3926
Chubru <sashach02@yandex.ru>
2021-10-22 04:12:55 +0300

    .

--- test 2-1/2/2/main.cpp
+++ test 2-1/2/2/main.cpp
@@ -5,13 +5,13 @@
 std::vector <int> storage;
 
 void f(int dat, int depth, int n = 0){
-    if(dat==0)
+    if(dat == 0)
         return;
 
     static int originalValue;
-    if(n==0)
+    if(n == 0)
         originalValue=dat;
-    for(int i=0;i<dat;i++){
+    for(int i = 0;i < dat;i++){
         storage.push_back(dat-i);
         if(n < depth){
             int sum = 0;
---
+++ test 2-1/4/3/main.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+std::vector <int> storage;
+
+
+void f(int numberItems,int n = 0){
+
+    if(n == numberItems){
+        for(int i = 0;i < storage.size();i++){
+            std::cout<<storage[i];
+        }
+        std::cout<<std::endl;
+    }
+    else{
+        for(int i = 0;i < numberItems;i++){
+            if(std::find(storage.begin(), storage.end(), i)!=storage.end())
+                continue;
+            storage.push_back(i);
+            f(numberItems,k,n+1);
+            storage.pop_back();
+        }
+    }
+}
+
+int main() {
+    f(3);
+    return 0;
+}
