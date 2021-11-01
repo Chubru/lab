e3e7cef4b324f60a89623fc27a0b6ba50967f9fb
Chubru <sashach02@yandex.ru>
2021-11-02 02:24:08 +0300

    add move &&& vector of vector in Lab_17

--- Lab_17/Cvector.h
+++ Lab_17/Cvector.h
@@ -13,39 +13,57 @@ template <typename T>
 class Cvector {
 private:
     T* ptr;
-    size_t_ size=0;
-    size_t_ usage=0;
-    size_t_ incr=-1;
+    size_t_ size{10};
+    size_t_ usage{0};
 public:
-    Cvector(){size=10; ptr = new T[size]; return;}
-    Cvector(int size_){size=size_; ptr = new T[size]; return;}
-    Cvector(int size_,int incr_){size=size_; incr=incr_; ptr = new T[size]; return;}
+    Cvector(){ptr = new T[size]; return;}
 
-    void pushBack(T dat){
-        if(usage<size){
-            ptr[usage]=dat;
-            usage++;
-        }
-        else{
-            if(incr!=-1) {
-                T* tmpPtr = new T[size+incr];
-                for(int i=0;i<size;i++){
-                    tmpPtr[i]=ptr[i];
-                }
-                delete[] ptr;
-                ptr=tmpPtr;
-                size = size + incr;
-            }
-            else{
+    Cvector(Cvector& v): size (v.szie), usage(v.usage){
+        ptr = new T[size];
+        for (int i=0; i < usage; i++) ptr[i] = v.ptr[i];
+    }
+
+    Cvector(Cvector&& v): ptr(v.ptr), size (v.size), usage(v.usage){
+        v.size=10;
+        v.usage = 0;
+        v.ptr = new T[size];
+    }
+
+    Cvector& operator=(Cvector&& v){
+        delete[] ptr;
+        ptr=v.ptr;
+        size=v.size;
+        usage=v.usage;
+
+        v.size=10;
+        v.usage = 0;
+        v.ptr = nullptr;
+        return *this;
+    }
+
+    Cvector<T>& operator= (const Cvector<T>& v){
+        delete[] ptr;
+        size=v.size;
+        ptr = new T[size];
+        usage = v.usage;
+        for (int i=0; i< usage; i++) ptr[i] = v.ptr[i];
+        return *this;
+    }
+    ~Cvector(){delete[] ptr;}
+
+    void pushBack(const T& dat){
+        if(usage + 1 >= size){
                 T* tmpPtr = new T[size*2];
-                for(int i=0;i<size;i++){
-                    tmpPtr[i]=ptr[i];
+                for(int i = 0;i < size ; i++){
+                    tmpPtr[i] = ptr[i];
                 }
                 delete[] ptr;
-                ptr=tmpPtr;
+                ptr = tmpPtr;
                 size = size * 2;
-            }
         }
+        ptr[usage] = dat;
+        usage++;
+
     }
 
     T& operator [](size_t_ index){
@@ -62,22 +80,21 @@ public:
     }
 
     void insert(size_t_ i, const T& value){
-        if( i > usage || i < 0 ){
-            throw "invalid index";
-        }
-        if(usage<size-1) {
-            T *tmpPtr = new T[size + incr];
-            for (int i = 0; i < size; i++) {
-                tmpPtr[i] = ptr[i];
+        if(usage == size){
+            T* tmpPtr = new T[size*2];
+            for(int i=0;i < size;i++){
+                tmpPtr[i]=ptr[i];
             }
             delete[] ptr;
-            ptr = tmpPtr;
-            size = size + incr;
-        }
-        for(size_t_ i=usage-1;i>=i;i++){
-            ptr[i+1]=ptr[i];
+            ptr=tmpPtr;
+            size = size * 2;
+            }
+        for(int j = i;j <= usage ;j++){
+            ptr[j+1]=ptr[j];
         }
         ptr[i]=value;
+        usage++;
+
     }
 
     void erase(size_t_ id){
@@ -91,6 +108,18 @@ public:
         ptr[id]=0;
         usage--;
     }
+
+    bool operator == (const Cvector<T>& v){
+        if( usage != v.usage)
+            return false;
+
+        for(int i=0;i < size;i++){
+            if( ptr[i] != v.ptr[i])
+                return false;
+        }
+        return true;
+    }
+
 
 };
 
--- Lab_17/main.cpp
+++ Lab_17/main.cpp
@@ -7,15 +7,31 @@ struct Spoint{
     int y;
 };
 
+Cvector<Spoint> genPointList (){
+    Cvector<Spoint> pl;
+    pl.pushBack(Spoint{3,5});
+    pl.pushBack(Spoint{4,6});
+    return pl;
+}
 int main() {
     try {
         //Cvector<std::string>dat;
         //dat.pushBack("qqqq");
         //std::cout<<dat[0];
-        Cvector<int> dat;
+        Cvector<Cvector <int>>dat;
         //dat[-1]=0;
-        for (int i = 0; i < 100; i++) {
-            dat.pushBack(i);
+        for (int ii=0; ii<15;++ii){
+            Cvector <int> d;
+            for (int i = 0; i < 12; i++) {
+                d.pushBack(i+ii);
+            }
+            dat.pushBack(d);
+        }
+        for (int ii=0; ii<15;++ii){
+            for (int i = 0; i < 12; i++) {
+                std::cout<<dat[ii][i]<<" ";
+            }
+            std::cout<<std::endl;
         }
         //dat.erase(-1);
 
@@ -25,6 +41,11 @@ int main() {
         pointList.pushBack(p);
         auto [x,y]=pointList[0];
         std::cout<<"x="<<x<<"y:"<<y;
+
+        pointList = genPointList();
+        Spoint pnt = pointList[0];
+        std::cout<<"x="<<pnt.x<<"y:"<<pnt.y;
+
     }
     catch (...){
         std::cout<<"invalid index";
